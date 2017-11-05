fd3a01da93ac929071e2967eb2db0b05245e1d34
Dr-Kaos <30758750+Dr-Kaos@users.noreply.github.com>
2017-11-05 15:13:16 +0000

    Add files via upload

---
+++ web_scrap.py
@@ -0,0 +1,12 @@
+import re
+import requests
+
+URL = "https://github.com"
+
+req = requests.get(URL)
+text = req.text()
+i = 0
+
+for m in re.finditer(r"https?[^\"]*", text):
+    print("%d=> %d-%d: %s" % (i, m.start(), m.end(), m.group(0)))
+    i += 1
