538b7783852194194e1cd9417645594dbdea83c2
Dr-Kaos <30758750+Dr-Kaos@users.noreply.github.com>
2017-11-05 19:01:50 +0000

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,12 +1,19 @@
 import re
 import requests
 
-URL = "https://github.com"
+def get(URL, i):
+    req = requests.get(URL)
 
-req = requests.get(URL)
-text = req.text
-i = 0
-
-for m in re.finditer(r"https?[^\"]*", text):
-    print("%d=> %d-%d: %s" % (i, m.start(), m.end(), m.group(0)))
-    i += 1
+    if req.status_code !== 200:
+        print("Status code different then 200!")
+    else:
+        text = req.text
+        file = open("file"+str(i)+".html",'w')
+        file.write(text)
+        file.close()
+        for m in re.finditer(r"https?://[^\"]*", text):
+            print("%d=> %d-%d: %s" % (i, m.start(), m.end(), m.group(0)))
+            i += 1
+            get(m.group(0), i)
+            
+get("http://www.example.com", i=0)
